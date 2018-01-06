28ae99e5fda5cdae6a95e6bbfb918bff86cc4847
ocheja <ocheja.patrick@gmail.com>
2018-01-07 06:28:54 +0900

    Modify contracts

--- src/app/services/contract/index-contract.service.ts
+++ src/app/services/contract/index-contract.service.ts
@@ -14,6 +14,7 @@ import {ReplaySubject} from 'rxjs/ReplaySubject';
 export class IndexContractService implements OnInit {
   userIndexContractOM: any;
   providerIndexContractOM: any;
+  llpc: any;
   provider: any;
   accountType: string = "Learner";
 
@@ -27,6 +28,8 @@ export class IndexContractService implements OnInit {
     this.userIndexContractOM.setProvider(this.provider);
     this.providerIndexContractOM = contract(ProviderIndexContract);
     this.providerIndexContractOM.setProvider(this.provider);
+    this.llpc = contract(LearnerLearningProviderContract);
+    this.llpc.setProvider(this.provider);
   }
 
   getRecordsByLearningProvider(index_contract_address, learning_provider, start = 0, end = 10): Observable<any> {
@@ -86,6 +89,16 @@ export class IndexContractService implements OnInit {
   getMyLearningProviders(index_contract_address, owner_address): Observable<any> {
     const result = new ReplaySubject();
     const currentIndexContract = this.accountType === 'Learner' ? this.userIndexContractOM : this.providerIndexContractOM;
+    this.llpc.at("0xa108e693191db19e6d8e5055fa98107bfcc69145").then( cotrct => {
+      cotrct.getOwner().then(res => {
+        console.log("Owner:::", res);
+        result.next(res);
+      });
+      cotrct.getRecordType().then(res => {
+        console.log("ReType:::", res);
+        result.next(res);
+      });
+    });
     currentIndexContract.at(index_contract_address).then( indexContract => {
       indexContract.getLearningProviders().then( response => {
         console.log(response);
@@ -93,6 +106,11 @@ export class IndexContractService implements OnInit {
       }).catch(error => {
         console.log(error);
         result.error(error);
+      });
+      indexContract.getLearningRecordsByProvider("0xe715f10de7cfcca2eb155ef87eea8c832bffcd78", 0, 4).then( response => {
+        console.log("2222222", response);
+      }).catch(error => {
+        console.log("22222eee:", error);
       });
     }).catch(error => {
       console.log(error);
