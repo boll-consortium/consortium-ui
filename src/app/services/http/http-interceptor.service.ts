44e1e1a7e175f8eedcc257ff6da43bfe96c3baf7
patrick <ocheja.patrick@gmail.com>
2019-01-09 17:13:18 +0900

    fix NPE

--- src/app/services/http/http-interceptor.service.ts
+++ src/app/services/http/http-interceptor.service.ts
@@ -1,19 +1,20 @@
 import {Injectable} from "@angular/core";
 import Axios from "axios";
 import {isNullOrUndefined} from "util";
+import {SessionStateService} from "../global/session-state.service";
 
 @Injectable()
 export class HttpInterceptorService {
   public axiosInstance: any;
 
-  constructor() {
+  constructor(private sessionStateService: SessionStateService) {
     this.axiosInstance = Axios.create();
     this.axiosInstance.interceptors.response.use(function (response) {
       // Do something with response data
       console.log("HTTP Connection intercepted...");
       if (((!isNullOrUndefined(response.error) && response.error.code === 401) || response.data['sessionExpired'])
         && (isNullOrUndefined(response.url) || response.url.indexOf('/login') === 0)) {
-        this.sessionStateService.clearAll();
+        sessionStateService.clearAll();
         if (this.inIframe()) {
           window.location.reload(false);
           return null;
