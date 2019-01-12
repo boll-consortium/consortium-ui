8368fb43784add9e120527350cd96dda607279e9
patrick <ocheja.patrick@gmail.com>
2019-01-12 20:28:24 +0900

    clean up

--- src/app/components/learning-records/learning-records.component.ts
+++ src/app/components/learning-records/learning-records.component.ts
@@ -55,9 +55,7 @@ export class LearningRecordsComponent implements OnInit {
       if (params['view'] !== null && params['view'] !== undefined) {
         console.log(this.currentView);
         this.currentView = params['view'];
-        console.log(this.currentView);
       } else {
-        console.log(this.route.snapshot.params['view']);
       }
     });
   }
@@ -78,7 +76,6 @@ export class LearningRecordsComponent implements OnInit {
       this.noAccount = true;
       console.log("no account");
     } else if (this.sessionStateService.getUser() !== null && this.sessionStateService.getUser()['accounts'].length > 0) {
-      console.log("loading index contract");
       this.loadIndexContractAddress(this.sessionStateService.getUser()['accounts'][0], null);
     }
   }
@@ -86,7 +83,6 @@ export class LearningRecordsComponent implements OnInit {
   loadLearningProviders(indexContractAddress, ownerAddress) {
     this.indexContractService.getMyLearningProviders(indexContractAddress, ownerAddress).subscribe(response => {
       this.rawProviders = response;
-      console.log("providers ", response);
       this.loadLearningRecords('provider', this.rawProviders);
       this.loadProviders(response);
     });
@@ -105,7 +101,6 @@ export class LearningRecordsComponent implements OnInit {
       providers.forEach((providerAddress, index) => {
         if (providerAddress !== null && providerAddress !== undefined) {
           this.indexContractService.getRecordsByLearningProvider(this.indexContractAddress, providerAddress).subscribe(records => {
-            console.log("Records ", records);
             if (records !== null && records.length > 0 && records[0] !== "0x0000000000000000000000000000000000000000") {
               this.learningRecords = records;
               for (let i = 0; i < records.length; i++) {
@@ -122,10 +117,8 @@ export class LearningRecordsComponent implements OnInit {
       });
     } else {
       if (this.recordType !== undefined && this.recordType !== null) {
-        console.log("sssssssss ", this.recordType, this.sessionStateService.recordTypesToUniqueId[this.recordType]);
         this.indexContractService.getRecordsByType(this.indexContractAddress,
           this.sessionStateService.recordTypesToUniqueId[this.recordType]).subscribe(records => {
-          console.log(records, this.currentView);
           if (records !== null && records.length > 0 && records[0] !== "0x0000000000000000000000000000000000000000") {
             this.learningRecords = records;
             for (let i = 0; i < records.length; i++) {
@@ -146,12 +139,6 @@ export class LearningRecordsComponent implements OnInit {
     this.registrarService.getIndexContract(ownerAddress).subscribe(response => {
       if (response && response !== "0x0000000000000000000000000000000000000000") {
         this.indexContractAddress = response;
-        console.log("User is:::", ownerAddress, "Index Contract:::", response);
-        /*this.indexContractService.insertRecordTest(this.indexContractAddress, "").subscribe(response2 => {
-          console.log("CCCCCCCCCCCC" + response2);
-        }, error2 => {
-          console.log("Error:::::" + error2);
-        });*/
         this.loadLearningProviders(response, this.sessionStateService.getUser()['accounts'][0]);
       } else {
         console.log(response, "no index contract", registrarAddress, ownerAddress);
@@ -160,23 +147,19 @@ export class LearningRecordsComponent implements OnInit {
   }
 
   setShowAddForm(show) {
-    console.log('sssss', show);
     this.showAddForm = show;
   }
 
   loadLearningRecordInfo(recordAddress) {
     this.indexContractService.loadLearningRecordInfo(recordAddress).subscribe(response => {
       this.recordInfos[recordAddress] = response;
-      console.log("XXXXXXXXVVVVV", this.recordInfos);
     }, error => {
       console.log(error);
     });
   }
 
   loadLearningRecordDeepInfo(recordAddress, recordSize) {
-    console.log("Record now is of size: ", parseInt(recordSize, 16), parseInt(recordSize, 10), recordAddress, recordSize);
     this.indexContractService.getRawLearningRecord(recordAddress, 0, parseInt(recordSize, 10)).subscribe(response => {
-      console.log("W::: ", response);
       response.forEach((record, index) => {
         if (!isNullOrUndefined(this.selectedSchool)) {
           if (this.selectedSchool === record['writer']) {
@@ -196,13 +179,11 @@ export class LearningRecordsComponent implements OnInit {
   public getRecord(info) {
     const url = info.queryHash;
     this.httpInterceptorService.axiosInstance.get(url).then(response => {
-      console.log(response.data);
       info['rawData'] = new JSONFormatter(response.data, Infinity).render();
       if (isNullOrUndefined(this.rawInfos)) {
         this.rawInfos = [];
       }
       this.rawInfos.push(info);
-      console.log("After", this.rawInfos);
     }).catch(error => {
       console.log(error);
     });
@@ -235,7 +216,6 @@ export class LearningRecordsComponent implements OnInit {
       if (isNullOrUndefined(this.schoolsChecked[schoolAddress])) {
         this.schoolsChecked[schoolAddress] = true;
         this.dbService.getSchool(this.user['accounts'][0], this.user['token'], schoolAddress).subscribe(response => {
-          console.log("I have got one more school...", response);
           school = this.sessionStateService.getSchool(schoolAddress);
         });
       }
