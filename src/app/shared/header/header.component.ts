7fac2ccc92c73822f7741ca08b3f3fb0bd861170
patrick <ocheja.patrick@gmail.com>
2019-02-05 17:06:09 +0900

    #13 implement boll id card view and download

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,6 +1,6 @@
-import {AfterViewChecked, ChangeDetectorRef, Component, OnChanges, OnInit} from '@angular/core';
-import { ReplaySubject } from 'rxjs/ReplaySubject';
+import {AfterViewChecked, ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {SessionStateService} from "../../services/global/session-state.service";
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-header',
@@ -9,18 +9,34 @@ import {SessionStateService} from "../../services/global/session-state.service";
 })
 export class HeaderComponent implements OnInit, AfterViewChecked {
   public loggedIn: boolean;
+  private bollAddress: string;
   constructor(public sessionStateService: SessionStateService,
               private cdRef: ChangeDetectorRef) { }
 
   ngOnInit() {
     this.loggedIn = this.sessionStateService.getUser() !== null;
+
+    if (this.loggedIn) {
+      this.bollAddress = this.sessionStateService.getUser()['accounts'][0];
+    }
   }
 
   ngAfterViewChecked() {
     this.loggedIn = this.sessionStateService.getUser() !== null;
+
+    if (this.loggedIn) {
+      this.bollAddress = this.sessionStateService.getUser()['accounts'][0];
+    }
+
     this.cdRef.detectChanges();
   }
 
+
+  downloadIDCard() {
+    html2canvas('#bollIdCard').then(function (canvas) {
+      window.open(canvas.toDataURL('image/png'));
+    });
+  }
 
 
 }
